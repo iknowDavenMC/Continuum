9051e0895620aef280511a02527c704ad0799c50
rphinger@hotmail.com <rphinger@hotmail.com@3e005681-7d36-b2ff-e241-174def9b6c9e>
2012-07-27 17:21:49 +0000

    GameBackground class

---
+++ trunk/paranothing/paranothing/paranothing/GameBackground.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace paranothing
+{
+    class GameBackground
+    {
+        # region Attributes
+
+        private Texture2D backgroundTexture;
+        private Rectangle backgroundRect;
+
+        # endregion
+
+        # region Constructor
+
+        public GameBackground(Texture2D inTexture, Rectangle inRect)
+        {
+            backgroundTexture = inTexture;
+            backgroundRect = inRect;
+        }
+
+        # endregion
+
+        # region Methods
+
+        //Accessor
+        public Rectangle BackgoundRectangle
+        {
+            get
+            {
+                return backgroundRect;
+            }
+        }
+
+        //Update
+        public virtual void Update(Game1 game, KeyboardState keys)
+        {
+            //Presss enter to continue
+            if (keys.IsKeyDown(Keys.Space))
+            {
+                game.GameState = GameLevel.Level;
+           }
+        }
+
+        //Draw
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(backgroundTexture, backgroundRect, Color.White);
+        }
+
+        # endregion
+    }
+}
