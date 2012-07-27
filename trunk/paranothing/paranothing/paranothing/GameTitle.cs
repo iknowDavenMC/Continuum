fa074251f8ee2291e03b6118a55f629df93a318c
rphinger@hotmail.com <rphinger@hotmail.com@3e005681-7d36-b2ff-e241-174def9b6c9e>
2012-07-27 17:21:12 +0000

    Title class

---
+++ trunk/paranothing/paranothing/paranothing/GameTitle.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace paranothing
+{
+    class GameTitle : GameBackground
+    {
+        # region Attribute
+
+        private Rectangle topTextRect;
+        private Rectangle bottomTextRect;
+
+        # endregion
+
+        # region Constructor
+
+        public GameTitle(Texture2D inTexture, Rectangle inRect)
+            : base(inTexture, inRect)
+        {
+            topTextRect = new Rectangle();
+            bottomTextRect = new Rectangle();
+        }
+
+        # endregion
+
+        # region Methods
+
+        //Accessors
+        public Rectangle TopTextRectangle
+        {
+            get
+            {
+                return topTextRect;
+            }
+        }
+        public void setTopTextRectangle(Vector2 inVec)
+        {
+            topTextRect.X = BackgoundRectangle.Center.X - (int)(inVec.X / 2);
+            topTextRect.Y = BackgoundRectangle.Top;
+            topTextRect.Width = (int)inVec.X;
+            topTextRect.Height = (int)inVec.Y;
+        }
+        public Rectangle BottomTextRectangle
+        {
+            get
+            {
+                return bottomTextRect;
+            }
+        }
+        public void setBottomTextRectangle(Vector2 inVec)
+        {
+            bottomTextRect.X = BackgoundRectangle.Center.X - (int)(inVec.X / 2);
+            bottomTextRect.Y = BackgoundRectangle.Bottom - (int)inVec.Y;
+            bottomTextRect.Width = (int)inVec.X;
+            bottomTextRect.Height = (int)inVec.Y;
+        }
+
+        //Update
+        public override void Update(Game1 game, KeyboardState keys)
+        {
+            //Presss enter to start
+            if (keys.IsKeyDown(Keys.Enter))
+            {
+                game.GameState = GameLevel.Description;
+            }
+        }
+
+        # endregion
+    }
+}
