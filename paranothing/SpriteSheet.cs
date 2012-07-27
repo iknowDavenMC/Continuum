6e417e3576881edd0c3ebce98d7d673cfefca9ae
alex2539 <alex2539@gmail.com>
2012-07-27 15:23:57 +0000

    Actually added files

---
+++ paranothing/SpriteSheet.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace paranothing
+{
+    /// <summary>
+    /// A Spritesheet utility class
+    /// </summary>
+    public class SpriteSheet
+    {
+        private Dictionary<String, List<int>> animList;
+        private List<Rectangle> sprites;
+        public Texture2D image { get; set; }
+
+        public SpriteSheet(Texture2D image)
+        {
+            this.image = image;
+            animList = new Dictionary<string, List<int>>();
+            sprites = new List<Rectangle>();
+        }
+
+        /// <summary>
+        /// Define a sprite within the spritesheet.
+        /// </summary>
+        /// <param name="bounds">A Rectangle specifying the position and dimensions of the sprite on the sheet.</param>
+        /// <returns>The index of the sprite added.</returns>
+        public int addSprite(Rectangle bounds)
+        {
+            sprites.Add(bounds);
+            return sprites.Count();
+        }
+
+        /// <summary>
+        /// Divides the sprite sheet into a grid with cells of equal size. Good for uniform sheets.
+        /// </summary>
+        /// <param name="rows">The number of rows in the sprite sheet</param>
+        /// <param name="columns">The number of columns in the sprite sheet</param>
+        /// <param name="limit">The maximum number of sprites in the sheet. 0 for no maximum.</param>
+        public void splitSheet(int rows, int columns, int limit)
+        {
+            if (rows <= 0 || columns <= 0)
+                return;
+            int width = (int)Math.Floor((float)image.Width / columns);
+            int height = (int)Math.Floor((float)image.Height / rows);
+            int count = 0;
+            for (int row = 0; row != rows; row++)
+            {
+                for (int col = 0; col != columns; col++)
+                {
+                    if (count < limit || limit <= 0)
+                    {
+                        sprites.Add(new Rectangle(width * col, height * row, width, height));
+                        count++;
+                    }
+                    else return;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Define an animation within the sprite sheet.
+        /// </summary>
+        /// <param name="name">The name of the animation (must be unique to the sprite sheet).</param>
+        /// <param name="spriteIndices">A List of the indices of the sprites in the animation.</param>
+        /// <returns>True if adding was successful, false otherwise.</returns>
+        public bool addAnimation(String name, List<int> spriteIndices)
+        {
+            if (animList.ContainsKey(name))
+                return false;
+            animList.Add(name, spriteIndices);
+            return true;
+        }
+
+        /// <summary>
+        /// Define an animation within the sprite sheet.
+        /// </summary>
+        /// <param name="name">The name of the animation (must be unique to the sprite sheet).</param>
+        /// <param name="spriteIndices">An array of the indices of the sprites in the animation.</param>
+        /// <returns>True if adding was successful, false otherwise.</returns>
+        public bool addAnimation(String name, int[] spriteIndices)
+        {
+            List<int> indexList = new List<int>();
+            foreach (int i in spriteIndices)
+            {
+                indexList.Add(i);
+            }
+            return addAnimation(name, indexList);
+        }
+
+        /// <summary>
+        /// Gets a list of sprite indices in an animation.
+        /// </summary>
+        /// <param name="name">The name of the animation.</param>
+        /// <returns>A list containing the indices of the sprites in the animation. If the animation does not exist, the list will be empty.</returns>
+        public List<int> getAnimation(String name)
+        {
+            List<int> animation = new List<int>();
+            if (animList.ContainsKey(name))
+                animList.TryGetValue(name, out animation);
+            return animation;
+        }
+
+        /// <summary>
+        /// Returns whether or not the spritesheet has an animation.
+        /// </summary>
+        /// <param name="name">The name of the animation to search for.</param>
+        /// <returns>True if the animation exists, false otherwise.</returns>
+        public bool hasAnimation(String name)
+        {
+            return animList.ContainsKey(name);
+        }
+
+        /// <summary>
+        /// Gets a list of the animations in the spritesheet.
+        /// </summary>
+        /// <returns>A List of the animations in the spritesheet.</returns>
+        public List<String> getAnimList()
+        {
+            Dictionary<String, List<int>>.KeyCollection keys = animList.Keys;
+            return keys.ToList();
+        }
+
+        /// <summary>
+        /// Gets a rectangle representing a sprite in the spritesheet with the specified index.
+        /// </summary>
+        /// <param name="index">The index of the sprite to look for.</param>
+        /// <returns>A Rectangle representing the position and dimension of the sprite within the sheet.</returns>
+        public Rectangle getSprite(int index)
+        {
+            if (index < 0 || index >= sprites.Count)
+                return new Rectangle();
+            return sprites.ElementAt(index);
+        }
+    }
+
+}
---
+++ paranothing/paranothing/paranothing/Drawable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace paranothing
+{
+    /// <summary>
+    /// An interface for any object that can be drawn.
+    /// The implementing class decides how it handles the drawing, but it must implement the draw() method.
+    /// </summary>
+    interface Drawable
+    {
+        /// <summary>
+        /// Return the object's image
+        /// </summary>
+        /// <returns>The texture of the object</returns>
+        Texture2D getImage();
+        /// <summary>
+        /// Draw the object
+        /// </summary>
+        /// <param name="renderer">The spritebatch with which to draw</param>
+        /// <param name="tint">A colour to use as a tint</param>
+        void draw(SpriteBatch renderer);
+    }
+}
