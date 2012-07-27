971c96fd3a4f2042b487eb915e9b56c5164c45ae
alex2539 <alex2539@gmail.com>
2012-07-27 16:25:03 +0000

    Figured out Boy movement speed, GameController is actually started.

--- paranothing/paranothing/paranothing/GameController.cs
+++ paranothing/paranothing/paranothing/GameController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace paranothing
@@ -9,5 +10,40 @@ namespace paranothing
     class GameController
     {
         public KeyboardState keyState;
+
+        private List<Updatable> updatableObjs;
+        private List<Drawable> drawableObjs;
+        public Boy player;
+        public enum GameState { Game };
+        public enum Direction { Left, Right, Up, Down }
+        public GameState state;
+
+        public GameController(Boy player)
+        {
+            this.player = player;
+            updatableObjs = new List<Updatable>();
+            drawableObjs = new List<Drawable>();
+            updatableObjs.Add(player);
+            state = GameState.Game;
+        }
+
+        public void updateObjs(GameTime time)
+        {
+            keyState = Keyboard.GetState();
+            foreach (Updatable obj in updatableObjs)
+            {
+                obj.update(time, this);
+            }
+        }
+
+        public void drawObjs(SpriteBatch renderer)
+        {
+            foreach (Drawable obj in drawableObjs)
+            {
+                obj.draw(renderer);
+            }
+            player.draw(renderer);
+        }
+
     }
 }
