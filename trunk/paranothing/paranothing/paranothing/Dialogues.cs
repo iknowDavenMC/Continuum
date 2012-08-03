28ad7f0e7b0c3932e71cf4b50ed2ceb60592ce1a
rphinger@hotmail.com <rphinger@hotmail.com@3e005681-7d36-b2ff-e241-174def9b6c9e>
2012-08-03 16:18:53 +0000

    Dialogues class : Audible, Interactive

---
+++ trunk/paranothing/paranothing/paranothing/Dialogues.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Audio;
+
+namespace paranothing
+{
+    class Dialogues : Audible, Interactive
+    {
+        # region Attributes
+
+        //Audible
+        private Cue dialogCue;
+
+        # endregion
+
+        # region Constructor
+
+        public Dialogues()
+        {
+
+        }
+
+        # endregion
+
+        # region Methods
+
+        //Audible
+        public Cue getCue()
+        {
+            return dialogCue;
+        }
+
+        public void setCue(Cue cue)
+        {
+            dialogCue = cue;
+        }
+
+        public void Play()
+        {
+
+        }
+
+        //Interactive
+        public void Interact(Boy player)
+        {
+            //player.state = Boy.BoyState.Dialog;
+            //player.X = X + 25;
+        }
+
+        # endregion
+    }
+}
