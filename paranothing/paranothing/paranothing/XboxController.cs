330b0eb2fba28dc42e1af3d543f8f354e6e03f20
rphinger <rphinger@hotmail.com>
2012-08-03 19:23:57 +0000

    Menu, Controller, HappyEnd classes : to be finished

---
+++ paranothing/paranothing/paranothing/XboxController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace paranothing
+{
+    class XboxController : GameBackground
+    {
+        # region Constructor
+
+        public XboxController(Texture2D inTexture, Rectangle inRect)
+            : base(inTexture, inRect)
+        {
+            
+        }
+
+        # endregion
+    }
+}
