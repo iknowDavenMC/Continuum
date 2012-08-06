2f3e70822a462567c0c38963906024fc46606c4e
alex2539 <alex2539@gmail.com>
2012-08-06 03:57:29 +0000

    Missing files are up.

---
+++ paranothing/paranothing/paranothing/Camera.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace paranothing
+{
+    class Camera : Updatable
+    {
+        private GameController control = GameController.getInstance();
+        public int X, Y, Width, Height;
+        public float scale;
+        public Camera(int X, int Y, int Width, int Height, float scale)
+        {
+            this.X = X;
+            this.Y = Y;
+            this.Width = Width;
+            this.Height = Height;
+            this.scale = scale;
+        }
+
+        public void update(GameTime time)
+        {
+            Boy player = control.player;
+            Level level = control.level;
+
+            X = (int)player.X - (int)(Width/ scale / 2);
+            Y = (int)player.Y - (int)(Height/ scale/ 2);
+
+            if (X > level.Width - Width/scale)
+                X = level.Width - (int)(Width/scale);
+            if (Y > level.Height - Height/scale)
+                Y = level.Height - (int)(Height/scale);
+            if (X < 0)
+                X = 0;
+            if (Y < 0 || level.Height < Height/scale)
+                Y = 0;
+        }
+    }
+}
---
+++ paranothing/paranothing/paranothing/SpriteSheetManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace paranothing
+{
+    class SpriteSheetManager
+    {
+        private Dictionary<string, SpriteSheet> sheetDict;
+        private static SpriteSheetManager instance;
+
+        public static SpriteSheetManager getInstance()
+        {
+            if (instance == null)
+                instance = new SpriteSheetManager();
+            return instance;
+        }
+
+        private SpriteSheetManager()
+        {
+            sheetDict = new Dictionary<string, SpriteSheet>();
+        }
+
+        public SpriteSheet getSheet(string name)
+        {
+            SpriteSheet sheet;
+            if (sheetDict.ContainsKey(name))
+                sheetDict.TryGetValue(name, out sheet);
+            else
+                sheet = null;
+            return sheet;
+        }
+
+        public void addSheet(string name, SpriteSheet sheet)
+        {
+            if (!sheetDict.ContainsKey(name))
+                sheetDict.Add(name, sheet);
+        }
+    }
+}
