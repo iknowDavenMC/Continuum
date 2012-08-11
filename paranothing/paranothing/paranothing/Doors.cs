1f85791128223e193cb698e57f2b0f1f185695c3
alex2539 <alex2539@gmail.com>
2012-08-11 22:09:42 +0000

    Maybe fixed chairs going through doors.

--- paranothing/paranothing/paranothing/Doors.cs
+++ paranothing/paranothing/paranothing/Doors.cs
@@ -1,4 +1,4 @@
-﻿using System;
+ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -190,7 +190,7 @@ namespace paranothing
         }
         public bool isSolid()
         {
-            return false;
+            return locked;
         }
 
         //Audible
